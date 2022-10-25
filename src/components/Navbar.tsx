64888640b8346088542047b876bfac7018106a0d
dario-macedo-nftStudios <dario.macedo@nftstudios.io>
2022-10-24 19:02:32 -0500

    first commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Navbar = () => {
+  console.log("new");
+
+  return (
+    <div
+      className="navbar shadow-lg border-b-2"
+      style={{ background: "rgba(0, 0, 0, 0.3)" }}
+    >
+      <a href="/" className="btn btn-ghost normal-case text-xl">
+        daisyUI
+      </a>
+    </div>
+  );
+};
+
+export default Navbar;
