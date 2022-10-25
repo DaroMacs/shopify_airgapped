e9fc25b7be7199d18e78a7562c6edf53e549df79
DaroMacs <dariomacedog@gmail.com>
2022-10-24 19:24:08 -0500

    Added wallet connection

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import React from "react";
+import { ConnectButton } from "@rainbow-me/rainbowkit";
 
 const Navbar = () => {
   return (
     <div
-      className="navbar shadow-lg border-b-2"
+      className="navbar shadow-lg border-b-2 justify-between "
       style={{ background: "rgba(0, 0, 0, 0.3)" }}
     >
       <a href="/" className="btn btn-ghost normal-case text-xl">
         daisyUI
       </a>
+      <ConnectButton />
     </div>
   );
 };
