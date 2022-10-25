8c885c467e795e7b96634de94bedfa30175cdb21
DaroMacs <dariomacedog@gmail.com>
2022-10-25 18:52:57 -0500

    Added components

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -19,8 +19,8 @@ const Stats = () => {
             ></path>
           </svg>
         </div>
-        <div className="stat-title">Total Likes</div>
-        <div className="stat-value text-primary">25.6K</div>
+        <div className="stat-title">Events Organized</div>
+        <div className="stat-value text-primary">6K+</div>
         <div className="stat-desc">21% more than last month</div>
       </div>
 
@@ -40,22 +40,18 @@ const Stats = () => {
             ></path>
           </svg>
         </div>
-        <div className="stat-title">Page Views</div>
-        <div className="stat-value text-secondary">2.6M</div>
+        <div className="stat-title">Attendees</div>
+        <div className="stat-value text-secondary">50K+</div>
         <div className="stat-desc">21% more than last month</div>
       </div>
 
       <div className="stat">
         <div className="stat-figure text-secondary">
-          <div className="avatar online">
-            <div className="w-16 rounded-full">
-              <img src="https://placeimg.com/128/128/people" alt="nichts" />
-            </div>
-          </div>
+          <div className="avatar online"></div>
         </div>
         <div className="stat-value">86%</div>
-        <div className="stat-title">Tasks done</div>
-        <div className="stat-desc text-secondary">31 tasks remaining</div>
+        <div className="stat-title">Events Increasing</div>
+        <div className="stat-desc text-secondary">In the last 4 months</div>
       </div>
     </div>
   );
