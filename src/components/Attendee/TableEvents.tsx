a477580c7a25359c6f0f90b1a6a2fe495847e5d3
DaroMacs <dariomacedog@gmail.com>
2022-10-28 14:54:04 -0500

    Loader added

--- src/components/Attendee/TableEvents.tsx
+++ src/components/Attendee/TableEvents.tsx
@@ -1,9 +1,6 @@
-import React, { useState } from "react";
-import individual from "../../shared/images/individual.webp";
-import { useEffect } from "react";
-import { Contract } from "ethers";
+import React from "react";
 import { shortenWalletAddress } from "../../utils/shortenWallet";
-import { Link } from "react-router-dom";
+import { InfinitySpin } from "react-loader-spinner";
 
 const TableEvents = ({
   nfts,
@@ -25,60 +22,63 @@ const TableEvents = ({
       <div className="bg-gray-800 px-3 rounded-xl mb-5 font-medium text-cyan-100 text-lg">
         <p>Your NFTs</p>
       </div>
+      {isLoading ? (
+        <InfinitySpin width="200" color="#4d97a9" />
+      ) : (
+        <table className="table text-gray-800 w-full">
+          <thead>
+            <tr>
+              <th></th>
 
-      <table className="table text-gray-800 w-full">
-        <thead>
-          <tr>
-            <th></th>
-
-            <th>Name</th>
-            <th>Address</th>
-            <th>Token</th>
-          </tr>
-        </thead>
-        <tbody>
-          <div />
-          {nfts?.ownedNfts.map((nft: any) => (
-            <tr className="hover">
-              <td>
-                <div className="flex items-center space-x-3">
-                  <div className="avatar">
-                    <div className="mask mask-squircle w-12 h-12">
-                      {nft?.media[0].gateway.includes(".mp4") ? (
-                        <iframe
-                          title="nftvid"
-                          className="w-full aspect-video aspect-square"
-                          src={nft?.media[0].gateway}
-                        ></iframe>
-                      ) : (
-                        <img
-                          src={nft?.media[0].gateway}
-                          alt="Avatar Tailwind CSS Component"
-                        />
-                      )}
+              <th>Name</th>
+              <th>Address</th>
+              <th>Token</th>
+            </tr>
+          </thead>
+          <tbody>
+            <div />
+            {nfts?.ownedNfts.map((nft: any) => (
+              <tr className="hover">
+                <td>
+                  <div className="flex items-center space-x-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle w-12 h-12">
+                        {nft?.media[0].gateway.includes(".mp4") ? (
+                          <iframe
+                            title="nftvid"
+                            className="w-full aspect-video aspect-square"
+                            src={nft?.media[0].gateway}
+                          ></iframe>
+                        ) : (
+                          <img
+                            src={nft?.media[0].gateway}
+                            alt="Avatar Tailwind CSS Component"
+                          />
+                        )}
+                      </div>
                     </div>
                   </div>
-                </div>
-              </td>
-              <td className="uppercase">
-                {nft?.contract.name !== undefined
-                  ? nft?.contract.name
-                  : nft?.rawMetadata.name.substring(0, 16)}
-              </td>
-              <td>
-                <a
-                  href={`https://polygonscan.com/address/${nft?.contract.address}`}
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  {shortenWalletAddress(nft?.contract.address)}
-                </a>
-              </td>
-              <td>{nft?.contract.tokenType}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                </td>
+                <td className="uppercase">
+                  {nft?.contract.name !== undefined
+                    ? nft?.contract.name
+                    : nft?.rawMetadata.name.substring(0, 16)}
+                </td>
+                <td>
+                  <a
+                    href={`https://polygonscan.com/address/${nft?.contract.address}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {shortenWalletAddress(nft?.contract.address)}
+                  </a>
+                </td>
+                <td>{nft?.contract.tokenType}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
